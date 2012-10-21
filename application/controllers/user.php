672b15fed006357c112933fa746aafdc205e1d96
airiza yuddha yulian <airiza666@gmail.com>
2012-10-21 20:03:11 +0700

    add login

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -3,10 +3,20 @@
 class User extends CI_Controller {
     
     function index($error=''){
+        if($error){$data['login_error']=$error;}else{$data['login_error']=$error;}
         $data['title'] = 'User Page';
         $data['description'] ='';
         $data['keywords']='';
         $data['main_content'] = 'user/login';
         $this->load->view('includes/template',$data);
+    }
+    function login_act(){
+        $this->load->model('user_model');
+        $this->form_validation->set_rules('username','Username','trim|required');
+        $this->form_validation->set_rules('password','Password','trim|required');
+        if($this->form_validation->run()==false){
+            $this->index();
+        }else{redirect('site/index');}
+          
     }
 }
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,1 +1,19 @@
-aaskoaoao 
+<?php
+echo form_fieldset('Login');
+echo '<ul class="errors">';
+$this->form_validation->set_error_delimiters('<li>','</li>');
+echo validation_errors();
+if($login_error){
+    echo '<li>'.$login_error.'</li>';
+}
+echo '</ul>';
+echo form_open('user/login_act');
+echo form_label('Username : ','username');
+echo form_input('username');
+echo '<br></br>';
+echo form_label('Password : ','password');
+echo form_input('password');
+echo '<br></br>';
+echo form_submit('submit','Login');
+echo form_close();
+?>
