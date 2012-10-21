a4a25a45d26536e88b5c167a21eed104032e68ff
airiza yuddha yulian <airiza666@gmail.com>
2012-10-21 20:05:40 +0700

    add login

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -12,7 +12,7 @@ echo form_label('Username : ','username');
 echo form_input('username');
 echo '<br></br>';
 echo form_label('Password : ','password');
-echo form_input('password');
+echo form_password('password');
 echo '<br></br>';
 echo form_submit('submit','Login');
 echo form_close();
