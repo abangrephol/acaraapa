7e98a45b3d1e20d0b4b73f62037fda8773176de0
AbangRephol <abang.zeze@gmail.com>
2012-10-21 18:04:07 +0700

    add file site
    none

---
+++ application/controllers/site.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+    
+    function index($error=''){
+        $data['title'] = 'Home Page';
+        $data['description'] ='';
+        $data['keywords']='';
+        $data['main_content'] = 'site/index';
+        $this->load->view('site/site',$data);
+    }
+}
